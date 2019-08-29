2331b82f3dc37f788ca5d8d2beb783f859316298
Bhuvana KA <bhuvanaka@Bhuvanas-MacBook-Air.local>
2019-08-29 13:15:19 +0530

    added the credentials as environment variables

---
+++ README.md
@@ -0,0 +1,30 @@
+Install python-dotenv
+
+pip install -U python-dotenv
+
+
+Install snowflake connector for python 
+
+pip install --upgrade snowflake-connector-python
+
+
+Environment Variables:
+
+├── venv
+├── .env
+
+Create a file .env and store the credentials
+
+SF_ACCOUNT    = <'account_name'>
+SF_USER       = <'username'>
+SF_WAREHOUSE  = <'warehouse_name'>
+SF_DATABASE   = <'database_name'>
+SF_SCHEMA     = <'schema_name'>
+SF_PASSWORD   = <'password'>
+
+Create a .gitignore file and add the following lines.
+
+# Ignore files related to credentials
+.env
+
+
---
+++ sf_connector.py
@@ -0,0 +1,51 @@
+import snowflake.connector
+import json
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+ 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+ 
+# Get the credentials from .env
+SF_ACCOUNT    = os.getenv('SF_ACCOUNT')
+SF_USER       = os.getenv('SF_USER')
+SF_WAREHOUSE  = os.getenv('SF_WAREHOUSE')
+SF_DATABASE   = os.getenv('SF_DATABASE')
+SF_SCHEMA     = os.getenv('SF_SCHEMA')
+SF_PASSWORD   = os.getenv('SF_PASSWORD')
+
+# fire up an instance of a snowflake connection
+connection = snowflake.connector.connect (
+account  = SF_ACCOUNT,
+user     = SF_USER,
+password = SF_PASSWORD,
+database = SF_DATABASE,
+schema   = SF_SCHEMA
+)
+
+cs = connection.cursor()
+
+try:
+
+ #SAMPLE CODE 1
+  for cur in cs.execute("SHOW tables"):
+    for ret in cur:
+      print(ret)
+
+  #SAMPLE CODE 2 
+  cs.execute(
+    "CREATE OR REPLACE TABLE "
+    "test_table_1(col1 integer, col2 string)")
+
+  " YOUR CODE HERE "
+  
+except Exception as e:
+  raise e
+finally:
+  cs.close()
+
+connection.close()
+
+
+
